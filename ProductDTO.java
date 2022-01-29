c2646a520ca7565a05f3f3ae7a27128bb534b93e
lareefaimran <48053004+lareefaimran@users.noreply.github.com>
2022-01-29 15:05:08 +0530

    Add files via upload

---
+++ ProductDTO.java
@@ -0,0 +1,60 @@
+package com.sheryians.major.DTO;
+
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.sheryians.major.model.Category;
+@Table
+public class ProductDTO {
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImageName() {
+		return imageName;
+	}
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+	private  Long id;
+	private String name;
+	private int categoryId;
+	private double price;
+	private double weight;
+	private String description;
+	private String imageName;
+}
